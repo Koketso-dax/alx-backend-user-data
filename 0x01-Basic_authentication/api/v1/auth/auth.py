92aa7007f56fc71c676f41e4e74e51814fd93ff8
Koketso-dax <lanchaster.k@gmail.com>
2024-11-12 14:10:18 +0200

    [Update] require auth for wildcards

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,15 +7,32 @@ from flask import request
 class Auth:
     """Auth class to handle authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Require authentication"""
+        """Require authentication
+
+        Args:
+            path (str): The path to check.
+            excluded_paths (List[str]): A list of paths to
+            exclude from authentication.
+        Returns:
+            bool: True if authentication is required, False otherwise.
+        """
         if path is None:
             return True
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
         if path[-1] != '/':
             path += '/'
-        if path in excluded_paths:
-            return False
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('*'):
+                # Remove the wildcard character
+                base_excluded_path = excluded_path[:-1]
+                if path.startswith(base_excluded_path):
+                    return False
+            else:
+                if path == excluded_path:
+                    return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
