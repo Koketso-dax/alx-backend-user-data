0e27723bb9e49bac593205522338fca12244212a
Koketso-dax <lanchaster.k@gmail.com>
2024-11-14 15:03:54 +0200

    [Fix] session db auth

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,51 +1,61 @@
 #!/usr/bin/env python3
-"""Session db module"""
+"""
+SessionDBAuth module
+"""
 from api.v1.auth.session_exp_auth import SessionExpAuth
-from datetime import datetime, timedelta
 from models.user_session import UserSession
+from models import storage
 
 
 class SessionDBAuth(SessionExpAuth):
-    """Session db auth class"""
+    """
+    SessionDBAuth class
+    """
     def create_session(self, user_id=None):
-        """Create session"""
+        """
+        Create and store a new instance of UserSession
+        and return the Session ID
+        """
         session_id = super().create_session(user_id)
-        if session_id:
-            user_session = UserSession(user_id=user_id, session_id=session_id)
-            user_session.save()
+        if session_id is None:
+            return None
+
+        user_session = UserSession(user_id=user_id, session_id=session_id)
+        storage.new(user_session)
+        storage.save()
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
-        """Get user id from session id"""
-        try:
-            sessions = UserSession.search(session_id=session_id)
-            if sessions:
-                current_time = datetime.now()
-                time_elapsed = timedelta(
-                    seconds=self.session_duration
-                )  # session_duration is inherited from SessionExpAuth
-                if sessions[0].created_at + time_elapsed < current_time:
-                    return None
-                return sessions[0].user_id
-        except Exception:
+        """
+        Return the User ID by requesting UserSession in the
+        database based on session_id
+        """
+        if session_id is None:
             return None
 
+        user_sessions = storage.all(UserSession).values()
+        for user_session in user_sessions:
+            if user_session.session_id == session_id:
+                return user_session.user_id
+
+        return None
+
     def destroy_session(self, request=None):
-        """Destroy session"""
+        """
+        Destroy the UserSession based on the Session ID from the request cookie
+        """
         if request is None:
             return False
+
         session_id = self.session_cookie(request)
-        if not session_id:
+        if session_id is None:
             return False
-        user_id = self.user_id_for_session_id(session_id)
-        if not user_id:
-            return False
-        user_session = UserSession.search(session_id=session_id)
-        if user_session:
-            user_session[0].remove()
-        try:
-            user_session.remove()
-            UserSession.save_to_file()
-        except Exception:
-            return False
-        return True
+
+        user_sessions = storage.all(UserSession).values()
+        for user_session in user_sessions:
+            if user_session.session_id == session_id:
+                storage.delete(user_session)
+                storage.save()
+                return True
+
+        return False
