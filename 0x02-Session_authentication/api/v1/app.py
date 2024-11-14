328f1d085cfca1294f0ecc6f2920c1bb502ea9da
Koketso-dax <lanchaster.k@gmail.com>
2024-11-14 10:39:20 +0200

    [Fix] return None

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -51,8 +51,8 @@ def filter_request() -> str:
         abort(401)
 
     if auth.authorization_header(request) is None\
-        or auth.session_cookie(request) is None:
-        abort(401)
+        and auth.session_cookie(request) is None:
+        return None, abort(401)
 
     if auth.current_user(request) is None:
         abort(403)
