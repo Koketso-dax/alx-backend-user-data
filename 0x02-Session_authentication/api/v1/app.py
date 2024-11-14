cbca26792bc9b5854dda3ac5546c5452b44f3d55
Koketso-dax <lanchaster.k@gmail.com>
2024-11-14 10:19:35 +0200

    [Fix] pep 8 compliance

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -51,7 +51,7 @@ def filter_request() -> str:
         abort(401)
 
     if auth.authorization_header(request) is None\
-            or auth.session_cookie(request) is None:
+        or auth.session_cookie(request) is None:
         abort(401)
 
     if auth.current_user(request) is None:
