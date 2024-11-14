f6a46c1c3d5018c18847e8dc7279a141c5b9f5c6
Koketso-dax <lanchaster.k@gmail.com>
2024-11-14 10:47:14 +0200

    [Removed] extra conditinal check

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -44,11 +44,9 @@ def filter_request() -> str:
         '/api/v1/forbidden/',
         '/api/v1/auth_session/login/'
     ]
+
     if not auth.require_auth(request.path, excluded_paths):
         return
-
-    if auth.authorization_header(request) is None:
-        abort(401)
 
     if auth.authorization_header(request) is None\
         and auth.session_cookie(request) is None:
